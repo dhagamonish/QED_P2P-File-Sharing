6b79b831aa9324b6db3d14f3bd05edd25e203670
dhagamonish <dhagamonish@gmail.com>
2025-12-08 14:48:08 +0530

    Fix: Use PORT environment variable for deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { networkInterfaces } = require('os');
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
-const port = 3000;
+const port = process.env.PORT || 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
